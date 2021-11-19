1833f06c1a8083404a38988e629f9c401dbf1aec
Laura Lu <53279344+lauraluuu@users.noreply.github.com>
2021-11-19 22:28:01 +1300

    configure product detail page

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -10,18 +10,6 @@ export default function Catalog() {
         .then(response => response.json())
         .then(data => setProducts(data))
     }, [])
-  
-    // function addProduct() {
-    //   setProducts(prevState => [...prevState, 
-    //     {
-    //       id: prevState.length + 101,
-    //       name: 'product3' + (prevState.length + 1), 
-    //       price: (prevState.length * 100) + 100,
-    //       brand: 'some brand',
-    //       description: 'some desc',
-    //       pictureUrl: '/images/products/clinique-remover.jpeg'
-    //     }])
-    // }
 
     return (
         <>
